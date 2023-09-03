130f5427f5c22debbee2abad96c80bbb9d63e9b1
Marcos Paulo <marcos.paulo.mpsb99@gmail.com>
2023-09-02 23:03:02 -0300

    feature: criação do projeto

---
+++ src/main/java/br/com/marcos/portifolios/PortifoliosApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.portifolios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifoliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifoliosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcos/portifolios/PortifoliosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.portifolios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifoliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
