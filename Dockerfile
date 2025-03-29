74809c1e95ff73b8b6297c0563cce4475c1a49f1
marcos <marcos.paulo.mpsb99@gmail.com>
2025-03-29 20:39:59 -0300

    feat: design section inicio

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-# Use uma imagem oficial do OpenJDK como base
 FROM openjdk:17-jdk-slim
 
-# Defina o diretório de trabalho dentro do container
 WORKDIR /app
 
-# Copie o arquivo JAR gerado pelo Gradle para dentro do container
-COPY build/libs/br.com.marcos-0.0.1-SNAPSHOT.jar app.jar
+COPY build/libs/br.com.marcos-0.0.1.jar app.jar
 
-# Comando para rodar a aplicação
 ENTRYPOINT ["java", "-jar", "app.jar"]
 
-# Exponha a porta em que a aplicação estará rodando
 EXPOSE 8080
--- src/main/resources/static/js/theme.js
+++ src/main/resources/static/js/theme.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     let body = document.querySelector('body');
     let trilho = document.getElementById('trilho');
     let topoSite = document.getElementById('topo-do-site');
-    let especiliadades = document.getElementById('especiliadades');
+    let especialidades = document.getElementById('especialidades');
     let sobre = document.getElementById('sobre');
     let logoImage = document.getElementById('logoImage');
     let footerLogoImage = document.getElementById('footerLogoImage');
 
     if (trilho) {
         trilho.addEventListener('click', function () {
-            [trilho, topoSite, especiliadades, sobre, body].forEach(elemento => elemento.classList.toggle('dark'));
+            [trilho, topoSite, especialidades, sobre, body].forEach(elemento => elemento.classList.toggle('dark'));
 
             logoImage.src = trilho.classList.contains('dark') ? "/images/logo-dark.png" : "/images/logo-light.png";
             footerLogoImage.src = logoImage.src;
