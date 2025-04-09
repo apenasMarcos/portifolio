80a13cbfe0e539e187ae3e5568bcf8621154e999
Marcos <marcos.paulo.mpsb99@gmail.com>
2025-04-09 12:50:20 -0300

    feat: adicionando projeto 2

--- src/main/resources/static/js/projetos.js
+++ src/main/resources/static/js/projetos.js
@@ -1,5 +1,7 @@
 const modal = document.getElementById('modal');
 const closeBtn = document.getElementById('closeModal');
+const techContainer = modal.querySelector('.tech-icons');
+
 
 const projetos = {
     projeto1: {
@@ -7,21 +9,29 @@ const projetos = {
         descricao: 'Sistema de busca e filtragem de destinos turísticos com integração via API.',
         imagem: '/images/imagem.jpg',
         github: 'https://github.com/seurepo/viagens',
-        link: 'https://projeto-viagens.netlify.app'
+        link: 'https://projeto-viagens.netlify.app',
+        tecnologias: ['html', 'css', 'javascript']
     },
     projeto2: {
-        titulo: 'Conversor de moedas',
-        descricao: 'Dashboard para controle financeiro pessoal com gráficos interativos.',
+        titulo: 'Conversor Universal',
+        descricao: 'Aplicativo desktop feita em Java com Swing e tema escuro FlatLaf. Realiza conversão de:\n' +
+            ' 💰 Moedas (com cotação em tempo real via AwesomeAPI), \n' +
+            '📏 Comprimento, \n ' +
+            '⚖️ Massa, \n ' +
+            '🌡️ Temperatura e \n ' +
+            '💾 Armazenamento',
         imagem: '/images/projetos/conversor-moedas.png',
         github: 'https://github.com/apenasMarcos/conversor-de-moedas',
-        link: ''
+        link: '',
+        tecnologias: ['Java', 'Swing', 'FlatLaf', 'Gson', 'Maven', 'AwesomeAPI', 'Junit']
     },
     projeto3: {
         titulo: 'Projeto TREIX',
         descricao: 'Dashboard para controle financeiro pessoal com gráficos interativos.',
         imagem: '/images/imagem.jpg',
         github: 'https://github.com/seurepo/financeiro',
-        link: ''
+        link: '',
+        tecnologias: ['react', 'chartjs', 'sass']
     }
 };
 
@@ -46,6 +56,16 @@ document.querySelectorAll('.img-port').forEach(img => {
                 links[1].style.display = 'inline';
             } else {
                 links[1].style.display = 'none';
+            }
+
+            if (projeto.tecnologias && projeto.tecnologias.length > 0) {
+                techContainer.innerHTML = '';
+                projeto.tecnologias.forEach(tech => {
+                    const span = document.createElement('span');
+                    span.classList.add('tech-icon');
+                    span.textContent = tech;
+                    techContainer.appendChild(span);
+                });
             }
 
             modal.classList.add('active');
--- src/main/resources/static/js/theme.js
+++ src/main/resources/static/js/theme.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
     let especialidades = document.getElementById('especialidades');
     let sobre = document.getElementById('sobre');
     let scroll = document.getElementById('scroll-down');
+    let modalOverlay = document.getElementById('modal-split');
     let footerLogoImage = document.getElementById('footerLogoImage');
 
     if (trilho) {
         trilho.addEventListener('click', function () {
-            [trilho, topoSite, especialidades, sobre, scroll, body].forEach(elemento => elemento.classList.toggle('dark'));
+            [trilho, topoSite, especialidades, sobre, scroll, body, modalOverlay].forEach(elemento => elemento.classList.toggle('dark'));
 
             footerLogoImage.src = trilho.classList.contains('dark') ? "/images/logo-dark.png" : "/images/logo-light.png";
         });
