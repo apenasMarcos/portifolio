ac4c4d2ead4cb3791cc53bc29350d744790670b8
Marcos <marcos.paulo.mpsb99@gmail.com>
2025-04-15 17:37:01 -0300

    feat: adicionando projeto criando futuro

--- src/main/resources/static/js/projetos.js
+++ src/main/resources/static/js/projetos.js
@@ -1,32 +1,47 @@
 const modal = document.getElementById('modal');
 const closeBtn = document.getElementById('closeModal');
 const techContainer = modal.querySelector('.tech-icons');
+const topicosContainer = modal.querySelector('.modal-topicos');
 
 
 const projetos = {
     projeto1: {
-        titulo: 'Projeto Viagens',
-        descricao: 'Sistema de busca e filtragem de destinos turísticos com integração via API.',
-        imagem: '/images/imagem.jpg',
-        github: 'https://github.com/seurepo/viagens',
-        link: 'https://projeto-viagens.netlify.app',
-        tecnologias: ['html', 'css', 'javascript']
+        titulo: 'Mural Criando Futuro',
+        descricao: 'O Mural Criando o Futuro é uma aplicação web desenvolvida  com o objetivo de melhorar a comunicação entre escolas, creches e a comunidade escolar. A plataforma permite a publicação de avisos, notícias e cardápios de forma centralizada, acessível e intuitiva.\n' +
+            '\n' +
+            'O projeto foi desenvolvido com Java (Spring Boot) no backend e HTML/CSS/JS com Bootstrap no frontend, além de integrar tecnologias como Spring Security, Docker, Caffeine Cache, Supabase, Back4App e Imgur API.\n' +
+            '\n',
+        imagem: '/images/projetos/mural-criando-futuro.png',
+        github: 'https://github.com/apenasMarcos/Mural-Criando-o-Futuro',
+        link: '',
+        youtube: 'https://www.youtube.com/watch?v=0Bo-PrUSYiY',
+        tecnologias: ['Html', 'CSS', 'Javascript', 'Java', 'Spring Boot', 'Docker', 'Spring Security', 'Caffeine Cache', 'Supabase', 'Imgur API'],
+        topicos: [
+            'Mural de postagens com imagens',
+            'Gerenciamento de conteúdo por usuários autenticados',
+            'Exibição de cardápio semanal',
+            'Seção institucional com informações de contato',
+            'Interface amigável e responsiva',
+            'Sistema de cache para performance otimizada'
+        ]
     },
     projeto2: {
         titulo: 'Conversor Universal',
-        descricao: 'Aplicativo desktop feita em Java com Swing e tema escuro FlatLaf. Realiza conversão de:\n' +
-            ' 💰 Moedas (com cotação em tempo real via AwesomeAPI), \n' +
-            '📏 Comprimento, \n ' +
-            '⚖️ Massa, \n ' +
-            '🌡️ Temperatura e \n ' +
-            '💾 Armazenamento',
+        descricao: 'Aplicativo desktop feita em Java com Swing e tema escuro FlatLaf.',
         imagem: '/images/projetos/conversor-moedas.png',
         github: 'https://github.com/apenasMarcos/conversor-de-moedas',
         link: '',
-        tecnologias: ['Java', 'Swing', 'FlatLaf', 'Gson', 'Maven', 'AwesomeAPI', 'Junit']
+        tecnologias: ['Java', 'Swing', 'FlatLaf', 'Gson', 'Maven', 'AwesomeAPI', 'Junit'],
+        topicos: [
+            '💰 Conversor de Moedas (com cotação em tempo real via AwesomeAPI)',
+            '📏 Conversor de Comprimento',
+            '⚖️ Conversor de Massa',
+            '🌡️ Conversor de Temperatura',
+            '💾 Conversor de Armazenamento',
+        ]
     },
     projeto3: {
-        titulo: 'Projeto TREIX',
+        titulo: 'Em andamento',
         descricao: 'Dashboard para controle financeiro pessoal com gráficos interativos.',
         imagem: '/images/imagem.jpg',
         github: 'https://github.com/seurepo/financeiro',
@@ -58,6 +73,14 @@ document.querySelectorAll('.img-port').forEach(img => {
                 links[1].style.display = 'none';
             }
 
+            if (projeto.youtube) {
+                links[2].href = projeto.youtube;
+                links[2].textContent = '📹 Apresentação projeto';
+                links[2].style.display = 'inline';
+            } else {
+                links[2].style.display = 'none';
+            }
+
             if (projeto.tecnologias && projeto.tecnologias.length > 0) {
                 techContainer.innerHTML = '';
                 projeto.tecnologias.forEach(tech => {
@@ -66,6 +89,16 @@ document.querySelectorAll('.img-port').forEach(img => {
                     span.textContent = tech;
                     techContainer.appendChild(span);
                 });
+            }
+
+            if (projeto.topicos && projeto.topicos.length > 0 && topicosContainer) {
+                topicosContainer.innerHTML = '<h3>Principais funcionalidades:</h3><ul>';
+                projeto.topicos.forEach(topico => {
+                    topicosContainer.innerHTML += `<li>${topico}</li>`;
+                });
+                topicosContainer.innerHTML += '</ul>';
+            } else if (topicosContainer) {
+                topicosContainer.innerHTML = '';
             }
 
             modal.classList.add('active');
